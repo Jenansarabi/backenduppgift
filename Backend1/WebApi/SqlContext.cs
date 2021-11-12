ff7696d0a89c11062a9a8d17434d7a3db3d1f294
Jenan Sarabi <sarabi.ginan@gmail.com>
2021-11-12 14:34:18 +0100

    WebApi

---
+++ Backend1/WebApi/SqlContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi
+{
+    public class SqlContext : DbContext
+    {
+        private DbSet<User> users;
+
+        public SqlContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<SubCategory> SubCategories { get; set; }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        public virtual DbSet<User> Users { get => users; set => users = value; }
+
+
+
+    }
+
+
+}
